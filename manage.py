9d064b0e29f0da484d7a9c2e24665de7ef65a495
Юля <filkkina@gmail.com>
2025-06-14 14:23:37 +0900

    11

--- manage.py
+++ manage.py
@@ -1,20 +1,17 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_project.settings')
+    # Добавляем путь к корню проекта
+    sys.path.append(str(Path(__file__).resolve().parent))
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_project.ecommerce_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        raise ImportError(...) from exc
     execute_from_command_line(sys.argv)
 
 
